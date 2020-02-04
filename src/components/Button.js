51a06f19f13bf0f05b350233e8ca777219311cb7
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-04 12:09:28 -0500

    Reaction

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button type="button" className="btn btn btn-info">
+        click me
+      </button>
+    </div>
+  );
+};
+
+export default Button;
